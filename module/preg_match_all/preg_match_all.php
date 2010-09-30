67801bf3daf11a8e308a0fea3b928ce7bf2cbcb6
Thierry Poinot <tpoinot@clever-age.com>
2010-09-30 14:28:50 +0200

    rewrite a custom error handler instead of using output buffering

--- module/preg_match_all/preg_match_all.php
+++ module/preg_match_all/preg_match_all.php
@@ -1,30 +1,9 @@
 <?php
-
-//$h = haystack, $n = needle
-function strstrb($h,$n){
-    return array_shift(explode($n,$h,2));
-}
   $input = stripslashes(utf8_decode($_POST['input']));
   $regexp = stripslashes(utf8_decode($_POST['input-regexp']));
 
   $matches = array();
-
-  /**
-   * when error_reporting is setting to E_ALL or to E_WARNING
-   * Avoid showing errors with the path of the file when the regexp is not valid
-   * ob_get_contents() get the output error
-   * ob_end_clean() delete all output and avoid stopping script when warning occured
-   * finally, if $res is false, display the error without the part "found in __FILE__ on line __LINE__"
-   *
-   * it provide a support for the preg_match_all function and help user to found error in his regexp
-   */
-  ob_start();
-    $res = preg_match_all($regexp, $input, $matches);
-    $error = ob_get_contents();
-  ob_end_clean();
-  if ($res === false) {
-    die(strstrb($error, 'found in'));
-  }
+  preg_match_all($regexp, $input, $matches);
   ?>
   <table>
   <?php foreach($matches as $key => $matche): ?>
--- prepend.php
+++ prepend.php
@@ -4,3 +4,64 @@ define('SANDBOX_LIB_DIR', realpath('lib'));
 require_once(SANDBOX_LIB_DIR . DIRECTORY_SEPARATOR . 'variable.php');
 require_once(SANDBOX_LIB_DIR . DIRECTORY_SEPARATOR . 'file.php');
 require_once(SANDBOX_LIB_DIR . DIRECTORY_SEPARATOR . 'template.class.php');
+
+ini_set('docref_root', 'http://php.net/');
+set_error_handler("display_error");
+
+function display_error($errno, $errstr, $errfile, $errline, $errcontext){
+  $errtitle = array(
+      E_ERROR => 'error',
+      E_USER_ERROR => 'error',
+      E_NOTICE => 'notice',
+      E_USER_NOTICE => 'notice',
+      E_PARSE => 'parse',
+      E_WARNING => 'warning',
+      E_USER_WARNING => 'warning',
+  );
+
+  if (!defined('CUSTOM_ERROR_DONT_SHOW_FILE')) {
+    $pathinfo = pathinfo($errfile);
+    $dirname = $pathinfo['dirname'];
+    $basename = $pathinfo['basename'];
+    $extension = $pathinfo['extension'];
+    $filename = $pathinfo['filename'];
+
+    $filestr = '';
+    if (defined('CUSTOM_ERROR_SHOW_FULL_PATH_FILE')){
+      $filestr .= " in <span style='font-weight: bold;'>$errfile</span>";
+    }
+    else {
+      $filestr .= " in <span style='font-weight: bold;'>$basename</span>";
+    }
+    if (!defined('CUSTOM_ERROR_DONT_SHOW_LINE')) {
+      $filestr .= " on line <span style='font-weight: bold;'>$errline</span>";
+    }
+  }
+  $errtype = isset($errtitle[$errno]) ? $errtitle[$errno] : "error-$errno";
+  
+  echo "<div style='background: red; color: black;'>";
+  switch ($errno) {
+    case E_USER_ERROR:
+    case E_USER_WARNING:
+    case E_USER_NOTICE:
+    case E_NOTICE:
+    case E_WARNING:
+    case E_ERROR:
+    case E_PARSE:
+      echo "<span class='{$errtitle[$errno]}' style='text-transform: capitalize; font-weight: bold;'>{$errtitle[$errno]}:</span> $errstr";
+    break;
+    default:
+      echo "<span class='error-$errno' style='text-transform: capitalize; font-weight: bold;'>error n°$errno:</span> $errstr";
+    break;
+      
+  }
+  echo $filestr;
+  echo "</div>";
+
+  if($errno < E_NOTICE) {
+    exit;
+  }
+
+  /* Ne pas exécuter le gestionnaire interne de PHP */
+  return true;
+}
